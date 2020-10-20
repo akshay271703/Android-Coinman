7a4dfe05e56cfd7f6b33778baf4a2c7778bf9736
Akshay Bhargava <toakshay.official@gmail.com>
2020-10-20 22:32:09 +0530

    Add Coins and Bombs

--- core/src/com/coinman/game/CoinMan.java
+++ core/src/com/coinman/game/CoinMan.java
@@ -6,6 +6,11 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.xml.soap.Text;
+
 public class CoinMan extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
@@ -13,12 +18,24 @@ public class CoinMan extends ApplicationAdapter {
 	int height;
 	int width;
 
+	//Character
 	int manState=0;
 	int pause=0;
 	int manY;
 
-	float gravity = 0.2f;
+	//Physics
+	float gravity = 0.4f;
 	float velocity = 0;
+
+	//Coins and Bombs
+	ArrayList<Integer> coinX = new ArrayList<>();
+	ArrayList<Integer> coinY = new ArrayList<>();
+	Texture coin;
+	ArrayList<Integer> bombX = new ArrayList<>();
+	ArrayList<Integer> bombY = new ArrayList<>();
+	Texture bomb;
+	int coinCount;
+	int bombCount;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -32,12 +49,63 @@ public class CoinMan extends ApplicationAdapter {
 		width = Gdx.graphics.getWidth();
 
 		manY = height/2 -man[manState].getHeight();
+
+		coin = new Texture("coin.png");
+		bomb = new Texture("bomb.png");
+	}
+
+	public void genCoin()
+	{
+		Random random = new Random();
+		float h = random.nextFloat()* height;
+		coinY.add((int) h);
+		coinX.add(width);
+	}
+	public void genBomb()
+	{
+		Random random = new Random();
+		float h = random.nextFloat()* height;
+		bombY.add((int) h);
+		bombX.add(width);
 	}
 
 	@Override
 	public void render () {
 		batch.begin();
 		batch.draw(background,0,0,width,height);
+
+		//Generate Coins on screen
+		if(coinCount < 150){
+			coinCount++;
+		}else{
+			coinCount=0;
+			genCoin();
+		}
+
+		for(int i=0;i<=coinX.size()-1;i++)
+		{
+			batch.draw(coin, coinX.get(i) , coinY.get(i));
+			coinX.set(i, coinX.get(i)-4);
+		}
+
+		//Generate Bombs on screen
+		if(bombCount < 250){
+			bombCount++;
+		}else{
+			bombCount=0;
+			genBomb();
+		}
+
+		for(int i=0;i<=bombX.size()-1;i++)
+		{
+			batch.draw(bomb, bombX.get(i) , bombY.get(i));
+			bombX.set(i, bombX.get(i)-4);
+		}
+
+		if(Gdx.input.justTouched()){
+			velocity=-15;
+		}
+
 		if(pause<5){
 			pause++;
 		}else{
@@ -54,6 +122,9 @@ public class CoinMan extends ApplicationAdapter {
 
 		if(manY<=0){
 			manY=0;
+		}
+		if(manY == height-man[manState].getHeight()){
+			manY=height-man[manState].getHeight();
 		}
 
 		batch.draw(man[manState],width/2 - man[manState].getWidth()/2 , manY);
